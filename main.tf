ef89718916b36aa902279cb9f9ea00595a2befe3
shresis <162896197+shresis@users.noreply.github.com>
2024-08-02 03:54:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,3 +12,29 @@ module "vpc-module" {
     igw_name = "module-igw"
     public_rt_name = "module-rt"
 }
+
+
+resource "aws_s3_bucket" "remote-state" {
+    bucket = "terraform-state-file-26072024"
+    force_destroy = true 
+}
+
+resource "aws_s3_bucket_versioning" "s3-versioning" {
+    bucket = aws_s3_bucket.remote-state.id
+    versioning_configuration {
+      status = "Enabled"
+    }
+}
+
+resource "aws_dynamodb_table" "remote-statelock" {
+    name = "dynamodb-statelock"
+    billing_mode = "PROVISIONED"
+    read_capacity = 20
+    write_capacity = 20
+    hash_key = "LockID"
+
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+}
